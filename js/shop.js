416041548a09a8de0b4f9a1e50c51a6353cb7386
maru33luc <maru33luc@gmail.com>
2023-10-17 23:40:10 -0300

    Correccion detalles paginacion

--- js/shop.js
+++ js/shop.js
@@ -18,12 +18,12 @@ customSelect.addEventListener('change', function () {
 const minPriceInput = document.getElementById('min-price');
 const maxPriceInput = document.getElementById('max-price');
 
-minPriceInput.addEventListener('change', function() {
+minPriceInput.addEventListener('change', function () {
     validarYFormatearPrecios(minPriceInput);
-  });
-maxPriceInput.addEventListener('change', function() {
+});
+maxPriceInput.addEventListener('change', function () {
     validarYFormatearPrecios(maxPriceInput);
-  });
+});
 
 function validarYFormatearPrecios(input) {
 
@@ -31,12 +31,87 @@ function validarYFormatearPrecios(input) {
         alert('Por favor, ingrese valores numéricos válidos para los precios.');
         return;
     }
-    if(input.value < 0){
+    if (input.value < 0) {
         alert('Por favor, ingrese valores numéricos positivos para los precios.');
         return;
     }
     const minPriceValue = parseFloat(input.value);
     const minPriceFormatted = minPriceValue.toFixed(2);
     input.value = minPriceFormatted;
+
+}
+
+// --------------Pagination----------------
+
+const itemsPerPage = 9;
+const pageLinks = document.querySelectorAll('.page-link');
+const previousPage = document.getElementById('previous-page');
+const nextPage = document.getElementById('next-page');
+
+const allItems = document.querySelectorAll('.card-item');
+const totalItems = allItems.length;
+
+let currentPage = 0; 
+
+
+function showPage(pageNumber) {
+    const startIndex = pageNumber * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    allItems.forEach((item, index) => {
+        if (index >= startIndex && index < endIndex) {
+            item.style.position = 'relative';
+            item.style.display = 'block';
+        } else {
+            item.style.position = 'absolute';
+            item.style.display = 'none';
+        }
+    });
+
+    pageLinks.forEach((link, index) => {
+        if (index === pageNumber) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+    console.log(pageNumber);
+    if (pageNumber > 0) {
+        document.querySelector('ul.cards').style.marginTop = '-40px';
+    }else{
+        document.querySelector('ul.cards').style.marginTop = '50px';
+    }
+}
+
+function goToPage(pageNumber) {
+    if (pageNumber < 0) {
+        currentPage = 0; // No puede ser menor que 0
+    } else if (pageNumber >= Math.ceil(totalItems / itemsPerPage)) {
+        currentPage = Math.ceil(totalItems / itemsPerPage) - 1; // No puede ser mayor que el último índice de página
+    } else {
+        currentPage = pageNumber;
+    }
+
+    showPage(currentPage);
    
 }
+
+showPage(currentPage);
+
+pageLinks.forEach((link, index) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        goToPage(index);
+        
+    });
+});
+
+previousPage.addEventListener('click', (e) => {
+    e.preventDefault();
+    goToPage(currentPage - 1);
+});
+
+nextPage.addEventListener('click', (e) => {
+    e.preventDefault();
+    goToPage(currentPage + 1);
+});

77e817033c37027768a252f54e0353140946a47e
maru33luc <maru33luc@gmail.com>
2023-10-28 15:52:10 -0300

    Vistas adm

--- js/shop.js
+++ js/shop.js
@@ -1,28 +1,28 @@
-const minPriceInput = document.getElementById('min-price');
-const maxPriceInput = document.getElementById('max-price');
+// const minPriceInput = document.getElementById('min-price');
+// const maxPriceInput = document.getElementById('max-price');
 
-minPriceInput.addEventListener('change', function () {
-    validarYFormatearPrecios(minPriceInput);
-});
-maxPriceInput.addEventListener('change', function () {
-    validarYFormatearPrecios(maxPriceInput);
-});
+// minPriceInput.addEventListener('change', function () {
+//     validarYFormatearPrecios(minPriceInput);
+// });
+// maxPriceInput.addEventListener('change', function () {
+//     validarYFormatearPrecios(maxPriceInput);
+// });
 
-function validarYFormatearPrecios(input) {
+// function validarYFormatearPrecios(input) {
 
-    if (isNaN(input.value)) {
-        alert('Por favor, ingrese valores numéricos válidos para los precios.');
-        return;
-    }
-    if (input.value < 0) {
-        alert('Por favor, ingrese valores numéricos positivos para los precios.');
-        return;
-    }
-    const minPriceValue = parseFloat(input.value);
-    const minPriceFormatted = minPriceValue.toFixed(2);
-    input.value = minPriceFormatted;
+//     if (isNaN(input.value)) {
+//         alert('Por favor, ingrese valores numéricos válidos para los precios.');
+//         return;
+//     }
+//     if (input.value < 0) {
+//         alert('Por favor, ingrese valores numéricos positivos para los precios.');
+//         return;
+//     }
+//     const minPriceValue = parseFloat(input.value);
+//     const minPriceFormatted = minPriceValue.toFixed(2);
+//     input.value = minPriceFormatted;
 
-}
+// }
 
 // --------------Pagination----------------
 

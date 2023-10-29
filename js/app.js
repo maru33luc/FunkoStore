408567f69525f1a0fdd6d4f6eaf9ae00b7363847
maru33luc <maru33luc@gmail.com>
2023-10-29 16:05:38 -0300

    Estructura proyecto arreglada

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector(".navbar__menu")
-
-const iconoHamb = document.querySelector(".menu-icon")
-
-iconoHamb.addEventListener("click", () => {
-    menu.classList.toggle("navbar__menu")
-    menu.classList.toggle("navbar__menu-toogle")
-})
-

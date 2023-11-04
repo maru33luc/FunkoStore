ab34f04458ff136c0e6b7c7db0e65cea0113adf9
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-11-04 06:18:25 -0300

    [FEAT] login validation responsive

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,44 +1,57 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Auth } from 'firebase/auth';
-import { CartService } from 'src/app/services/cart.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+    selector: 'app-login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
-  loginForm: FormGroup;
 
-  constructor( private router: Router, private fb: FormBuilder,
-     private loginService:LoginService, private cartService: CartService) {
-    this.loginForm = this.fb.group({
-      email: [''],
-      password: [''],
-      remember: [false]
+    loginForm: FormGroup = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
     });
-  }
 
-  async login() {
-    try {
-      await this.loginService.login(this.loginForm.value.email, this.loginForm.value.password);
-      if(this.loginService.isUserLoggedIn()) {
-        this.loginService.isAdmin().subscribe(isAdmin => {
-          if (isAdmin) {
-            this.router.navigateByUrl('/admin');
-          }else{
-            this.router.navigateByUrl('/home');
-          }
-          
-        });
-      }
-      const carritoActual = await this.cartService.obtenerCarritoDeCompras();
-    } catch (error) {
-      console.error(error);
+    constructor(
+        private router: Router,
+        private fb: FormBuilder,
+        private loginService: LoginService,
+        private cartService: CartService
+    ) {}
+
+    validate(field: string, error: string) {
+        return (
+            this.loginForm.controls[field].getError(error) && 
+            this.loginForm.controls[field].touched);
     }
-  }
+
+    async login() {
+        if (this.loginForm.invalid) {
+            this.loginForm.markAllAsTouched();
+        } else {
+            try {
+                await this.loginService.login(
+                    this.loginForm.value.email,
+                    this.loginForm.value.password
+                );
+                if (this.loginService.isUserLoggedIn()) {
+                    this.loginService.isAdmin().subscribe((isAdmin) => {
+                        if (isAdmin) {
+                            this.router.navigateByUrl('/admin');
+                        } else {
+                            this.router.navigateByUrl('/home');
+                        }
+                    });
+                }
+                const carritoActual = await this.cartService.obtenerCarritoDeCompras();
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
 
 }

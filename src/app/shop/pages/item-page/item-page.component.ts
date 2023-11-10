e90e3df43cb82891617afb81391b7ce930a56284
maru33luc <maru33luc@gmail.com>
2023-11-10 17:43:19 -0300

    [FIX] productos relacionados x saga slider en itemPage

--- src/app/shop/pages/item-page/item-page.component.ts
+++ src/app/shop/pages/item-page/item-page.component.ts
@@ -3,6 +3,7 @@ import { ItemComponent } from '../../item/item.component';
 import { Observable } from 'rxjs';
 import { Funko } from 'src/app/interfaces/Funko';
 import { FunkosService } from 'src/app/services/funkos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-item-page',
@@ -14,9 +15,17 @@ export class ItemPageComponent {
     selectedItemLicence: string | undefined;
     selectedItemLicence$: Observable<string> | undefined;
 
-    constructor(private funkoService: FunkosService) {
-        this.funkoService.getFilteredFunkosObservable().subscribe(filteredFunkos => {
-            this.selectedItemLicence = filteredFunkos[0].licence;
+    constructor(private funkoService: FunkosService,
+        private activatedRoute: ActivatedRoute) {
+            this.obtenerIdItem();
+
+    }
+
+    obtenerIdItem() {
+        this.activatedRoute.paramMap.subscribe(async(params) =>  {
+            let id = params.get('id');
+            const resp = await this.funkoService.getFunko(parseInt(id || ''));
+            this.selectedItemLicence = resp?.licence;
         });
     }
 }

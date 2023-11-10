82a4891d743d42506d9a5e068dc708362b1813ff
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-11-10 09:01:58 -0300

    [STYLE] hero component

--- src/app/landing-page/landing-page-routing.module.ts
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AbousUsPageComponent } from './pages/abous-us-page/abous-us-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { TermsPageComponent } from './pages/terms-page/terms-page.component';
-import { AbousUsPageComponent } from './pages/abous-us-page/abous-us-page.component';
 
 const routes: Routes = [
     { path: '', component: HomePageComponent },
-    {path: 'nosotros', component: AbousUsPageComponent},
-    {path: 'contacto', component: ContactPageComponent},
-    {path: 'terminos', component: TermsPageComponent}
+    { path: 'nosotros', component: AbousUsPageComponent },
+    { path: 'contacto', component: ContactPageComponent },
+    { path: 'terminos', component: TermsPageComponent }
 ];
 
 @NgModule({
--- src/app/landing-page/pages/home-page/home-page.component.ts
+++ src/app/landing-page/pages/home-page/home-page.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import Glide from '@glidejs/glide';
-import { LoginService } from 'src/app/services/login.service';
-
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
-    title: string = 'ULTIMOS LANZAMIENTOS';
-
-    constructor(private loginService: LoginService) { }
-
-    async ngOnInit() {
-        // const resp = this.loginService.isUserLoggedIn();
-        // console.log(resp);
-        // const isAdmin = await  this.loginService.isAdmin();
-        // console.log(isAdmin);
-    }
-}
-
+export class HomePageComponent { }

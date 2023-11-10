655bd056861d65d249692488e530c288e839bad1
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-11-10 08:29:36 -0300

    [STYLE] slider component

--- src/app/shop/slider-glide/slider-glide.component.ts
+++ src/app/shop/slider-glide/slider-glide.component.ts
@@ -3,79 +3,73 @@ import { Funko } from 'src/app/interfaces/Funko';
 import { FunkosService } from 'src/app/services/funkos.service';
 
 @Component({
-  selector: 'app-slider-glide',
-  templateUrl: './slider-glide.component.html',
-  styleUrls: ['./slider-glide.component.css']
+    selector: 'app-slider-glide',
+    templateUrl: './slider-glide.component.html',
+    styleUrls: ['./slider-glide.component.css']
 })
 export class SliderGlideComponent implements OnInit {
-  lista: Funko[] = [];
-  itemsPerPage = 4;
-  currentPage = 0;
-  pages: number[] = [];
-  @Input () title: string | undefined;
-  @Input() filtroLista: string | undefined;
+    lista: Funko[] = [];
+    itemsPerPage = 4;
+    currentPage = 0;
+    pages: number[] = [];
+    @Input() title: string | undefined;
+    @Input() filtroLista: string | undefined;
 
-  constructor(private funkoService: FunkosService) {}
+    constructor(private funkoService: FunkosService) { }
 
-  ngOnInit() {
-    this.mostrarFunkos();
-    window.addEventListener('resize', () => {
-      this.updateItemsPerPage();
-    });
-  }
-
-  async mostrarFunkos() {
-    const response = await this.funkoService.getFunkos();
-    if (response != undefined) {
-      this.lista = response as Funko[];
-      // Aplicar filtro por algun criterio si está presente
-    if (this.filtroLista) {
-      this.lista = this.lista.filter(funko => funko.licence === this.filtroLista);
+    ngOnInit() {
+        this.mostrarFunkos();
+        window.addEventListener('resize', () => {
+            this.updateItemsPerPage();
+        });
     }
-      this.calculateTotalPages();
-    } else {
-      console.log('Error al mostrar los funkos');
+
+    async mostrarFunkos() {
+        const response = await this.funkoService.getFunkos();
+        if (response != undefined) {
+            this.lista = response as Funko[];
+            // Aplicar filtro por algun criterio si está presente
+            if (this.filtroLista) {
+                this.lista = this.lista.filter(funko => funko.licence === this.filtroLista);
+            }
+            this.calculateTotalPages();
+        } else {
+            console.log('Error al mostrar los funkos');
+        }
     }
-  }
 
-  calculateTotalPages() {
-    this.pages = Array(Math.ceil(this.lista.length / this.itemsPerPage)).fill(0).map((_, i) => i);
-  }
-
-  get pagedItems() {
-    const startIndex = this.currentPage * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.lista.slice(startIndex, endIndex);
-  }
-
-  changePage(newPage: number) {
-    if (newPage < 0) {
-      this.currentPage = 0;
-    } else if (newPage >= this.pages.length) {
-      this.currentPage = this.pages.length - 1;
-    } else {
-      this.currentPage = newPage;
+    calculateTotalPages() {
+        this.pages = Array(Math.ceil(this.lista.length / this.itemsPerPage)).fill(0).map((_, i) => i);
     }
-    return false;
-  }
 
-  updateItemsPerPage() {
-    if (window.innerWidth <= 1050 && window.innerWidth > 800) {
-      this.itemsPerPage = 3;
-    }else if (window.innerWidth <= 800 && window.innerWidth > 600) {
-      this.itemsPerPage = 2;
+    get pagedItems() {
+        const startIndex = this.currentPage * this.itemsPerPage;
+        const endIndex = startIndex + this.itemsPerPage;
+        return this.lista.slice(startIndex, endIndex);
     }
-    else if (window.innerWidth <= 600) {
-      this.itemsPerPage = 1;
+
+    changePage(newPage: number) {
+        if (newPage < 0) {
+            this.currentPage = 0;
+        } else if (newPage >= this.pages.length) {
+            this.currentPage = this.pages.length - 1;
+        } else {
+            this.currentPage = newPage;
+        }
+        return false;
     }
-     else {
-      this.itemsPerPage = 4;
+
+    updateItemsPerPage() {
+        if (window.innerWidth <= 1050 && window.innerWidth > 800) {
+            this.itemsPerPage = 3;
+        } else if (window.innerWidth <= 800 && window.innerWidth > 600) {
+            this.itemsPerPage = 2;
+        }
+        else if (window.innerWidth <= 600) {
+            this.itemsPerPage = 1;
+        }
+        else {
+            this.itemsPerPage = 4;
+        }
     }
-  }
-  
-  
-
-
-
-
 }

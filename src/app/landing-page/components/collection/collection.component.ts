4ef437aa2d8ac2d399c742e46a04f326c089033f
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-11-16 08:59:46 -0300

    [STYLE] agregadas animaciones en hero y collections

--- src/app/landing-page/components/collection/collection.component.ts
+++ src/app/landing-page/components/collection/collection.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FunkosService } from 'src/app/services/funkos.service';
+import AOS from 'aos';
 
 @Component({
     selector: 'app-collection',
     templateUrl: './collection.component.html',
     styleUrls: ['./collection.component.css']
 })
-export class CollectionComponent {
+export class CollectionComponent implements OnInit{
     
     constructor(private funkoService: FunkosService) {
     }
 
     ngOnInit() {
+        AOS.init();
         this.funkoService.emitirLanding("home");
     }
 
--- src/app/landing-page/components/hero/hero.component.ts
+++ src/app/landing-page/components/hero/hero.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
     selector: 'app-hero',
     templateUrl: './hero.component.html',
     styleUrls: ['./hero.component.css']
 })
-export class HeroComponent { }
+export class HeroComponent implements OnInit {
+
+    ngOnInit() {
+        AOS.init();
+    }
+}

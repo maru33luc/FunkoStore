83543b8a6c0e6b7dd05dd567632d7e3321ff6fbf
Federico Gil de Muro <federicogildemuro@gmail.com>
2023-12-12 16:05:01 -0300

    [STYLE] clear-cart-page

--- src/app/shop/pages/clear-cart-page/clear-cart-page.component.ts
+++ src/app/shop/pages/clear-cart-page/clear-cart-page.component.ts
@@ -1,16 +1,26 @@
 import { CartLocalService } from 'src/app/services/cart-local.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
     selector: 'app-clear-cart-page',
     templateUrl: './clear-cart-page.component.html',
     styleUrls: ['./clear-cart-page.component.css']
 })
-export class ClearCartPageComponent { 
-    constructor(private cartLocalService: CartLocalService) { 
+export class ClearCartPageComponent {
 
+    constructor(
+        private cartLocalService: CartLocalService,
+        private router: Router
+    ) {
         setTimeout(() => {
             this.cartLocalService.clearCart();
-        }, 400);
+            this.router.navigate(['/home']);
+        }, 5000);
+    }
+
+    ngOnInit() {
+        AOS.init();
     }
 }
